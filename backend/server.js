4949f934a9909f0406bb412f66f7d352f6576803
Loominarty <lukenis456@gmail.com>
2021-08-05 15:18:42 +0300

    Changes to work in production

--- backend/server.js
+++ backend/server.js
@@ -79,18 +79,17 @@ request(forecast_url, (error, response, body) =>{
   //console.log(body);
   })
 
-  
+
 
 
 })
-// Serve static files from the React frontend app
+
 app.use(express.static(path.join(__dirname, '../client/build')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 })
-
 
 
 
