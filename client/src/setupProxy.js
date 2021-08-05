b0c69b0d02059665afa1bb77363d3625c1ed1e52
Loominarty <lukenis456@gmail.com>
2021-08-05 18:10:44 +0300

    Minor change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
-      createProxyMiddleware(["/city", , "/weather", "/forecast"], { target: "http://localhost:8080" })
+      createProxyMiddleware(["/city", , "/weather", "/forecast","/forecast/selected_city","/forecast/:name"], { target: "http://localhost:8080" })
     );
   };
