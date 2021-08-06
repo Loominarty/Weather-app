27194e9bf589993b67f67ec7286fe06ade6d275b
Loominarty <lukenis456@gmail.com>
2021-08-06 12:43:41 +0300

    Test commit for deployment

--- client/src/InfoScreen.js
+++ client/src/InfoScreen.js
@@ -41,7 +41,7 @@ axios.post("https://weather-app-expressjs-server.herokuapp.com/forecast/selected
    
  }) 
 
-
+console.log(name);
 },[name])
 
 
@@ -50,7 +50,7 @@ axios.post("https://weather-app-expressjs-server.herokuapp.com/forecast/selected
     if(CityResponse!==null){
    axios.post({
    method: 'post',
-   url: "https://weather-app-expressjs-server.herokuapp.com/forecast/:name"
+   url: 'https://weather-app-expressjs-server.herokuapp.com/forecast/:name'
  })
  .then(res=>{
     
