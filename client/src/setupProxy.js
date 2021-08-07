e944aa95090d53dc16db348d79491403506a0419
Loominarty <lukenis456@gmail.com>
2021-08-07 16:37:21 +0300

    test commit for heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
-      createProxyMiddleware(["/city", "/weather", "/forecast", "/forecast/**"], { target: "https://weather-app-expressjs-server.herokuapp.com" })
+      createProxyMiddleware(["/city", "/weather","/forecast/selected_city", "/forecast", "/forecast/:name", "/forecast/Kretinga"], { target: "https://weather-app-expressjs-server.herokuapp.com" }),
+      
     );
   };
   
