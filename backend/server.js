7308f35c66acfbabe2e5c1a74e73cb0b4a3282da
Loominarty <lukenis456@gmail.com>
2021-08-08 11:06:02 +0300

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -77,8 +77,12 @@ request(forecast_url, (error, response, body) =>{
 
 })
 
-
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+});
 
 
 
