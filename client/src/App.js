038c2000017f1af7d1918ccf086c91b8c68c93b9
Lukas Brizgys <lukenis456@gmail.com>
2021-08-10 13:21:24 +0300

    Minor change in routes

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ function App() {
    <Route exact path="/">
 <Home/>
    </Route>
-   <Route path="/forecast/:name">
+   <Route exact path="/forecast/:name">
      <InfoScreen/>
    </Route>
  </Switch>
