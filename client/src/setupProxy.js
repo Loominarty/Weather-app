66624c1fd6e257454747c49991458f4cb508645d
Lukas Brizgys <lukenis456@gmail.com>
2021-08-12 12:40:49 +0300

    proxy for deployment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
-      createProxyMiddleware(["/city", "/weather","/forecast/selected_city", "/forecast", "/forecast/:name"], { target: "http://localhost:8080" }),
+      createProxyMiddleware(["/city", "/weather","/forecast/selected_city", "/forecast", "/forecast/:name"], { target: "https://weather-app-expressjs-server.herokuapp.com" }),
       
     );
   };
