a43524bb3e0d263b3f2e7f5552adf0ab14c8db6b
Lukas Brizgys <lukenis456@gmail.com>
2022-06-23 21:23:02 +0300

    add error messages

--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -1,15 +1,21 @@
 import React, {useEffect, useState, useRef} from 'react';
 import axios from 'axios'
-import { appendToLocalStorage, getLocalStorageItemCount, getLocalStorageItems, isCityInLocalStorage } from './utils/storage';
+import { appendToLocalStorage, getLocalStorageItemCount, getLocalStorageItems, isCityInLocalStorage, removeFromLocalStorage } from './utils/storage';
 import CityWeatherCard from './CityWeatherCard';
 import { TransitionGroup } from 'react-transition-group';
-
+import { CSSTransition } from "react-transition-group";
 const SideBar = () => {
     const [cities, setCities] = useState([]);
     const [isActive, setActive] = useState("false");
     const [isShown, setShown] = useState("false");
     const [error, setError] = useState(null);
     const form = useRef(null);
+    const removeCity = (cityToRemove) => {
+      const newCitiesArray = cities.filter((city) => city.name !== cityToRemove);
+      removeFromLocalStorage(cityToRemove);
+      setCities(newCitiesArray);
+    }
+    console.log(error);
     const updateCities = () => {
       axios.post(`${process.env.REACT_APP_SERVER_URL}/multipleWeather`,{cities: getLocalStorageItems()})
       .then((response) => {
@@ -44,10 +50,24 @@ const SideBar = () => {
       }
       updateCities();
     }, [])
+
     const handleSubmit = (e) => {
       e.preventDefault();
-      console.log("activated")
       const inputtedCity = form.current.value;
+      const cityCount = getLocalStorageItemCount();
+      if(!inputtedCity) {
+        setError('Įveskite miestą');
+        return;
+      }
+      if(isCityInLocalStorage(inputtedCity)){
+        setError('Miestas jau yra įtrauktas į sąrašą');
+        return;
+      }
+      if(cityCount >= 5){
+        setError('Negalima įvesti daugiau nei 5 miestų');
+        return;
+      }
+      setError(null);
       console.log(inputtedCity);
       if(inputtedCity) {
         axios.post(`${process.env.REACT_APP_SERVER_URL}/addCity`, {cityToAdd: inputtedCity, numberOfCities: getLocalStorageItemCount()})
@@ -66,22 +86,23 @@ const SideBar = () => {
     }
 return(
 <nav className="sidebar" id="sidebar">
-   <div className={isActive ? "hamburger-container" : "hamburger-container change"} id="hamburger" onClick={ToggleClass}>
+  <div className={isActive ? "hamburger-container" : "hamburger-container change"} id="hamburger" onClick={ToggleClass}>
    <div className="bar1"></div>
-  <div className="bar2"></div>
-  <div className="bar3"></div>
-   </div>
+   <div className="bar2"></div>
+   <div className="bar3"></div>
+  </div>
    
 <div className={isShown ? "sidebar-elements hide" : "sidebar-elements"} id="sidebar-elements" >
 <div className="city_form_container">
-      <form method="post" onSubmit={handleSubmit} id="form">
+      <form method="post" onSubmit={handleSubmit} id="form" className="city-submit-form">
         <div className="material-texfield">
-      <input ref={form} type='text' placeholder="" className="city_input" name="cityInput"/>
-      <label className="city_label">Įveskite miestą </label>
+          
+          <input ref={form} type='text' placeholder="" className="city_input" name="cityInput"/>
+          <label className="city_label">Įveskite miestą </label>   
       </div>
       <button type="submit" className="submit_city"> <i className="fas fa-plus"></i><span> Pridėti miestą</span> </button>
       </form>
-      
+      <div className="city-input-error">{error && error}</div>
     </div>
 
 
@@ -93,15 +114,13 @@ return(
   <TransitionGroup  className="my-card">
     {cities.length > 0 &&
       cities.map((city) => (
-        <CityWeatherCard key={city.id} city={city}/>
+        <CSSTransition classNames="card" timeout={500} key={city.id}>
+          <CityWeatherCard key={city.id} city={city} removeCity={removeCity}/>
+        </CSSTransition>
       ))
     }
   </TransitionGroup>
 </div>
-{/* <CityList city={city}/> */}
-
-
-
 </div>
 </nav>  
 );
